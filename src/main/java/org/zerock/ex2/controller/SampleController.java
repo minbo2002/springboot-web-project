dbab4e70fbc84f9374298dd4adde1aa13f335134
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-05-25 21:51:14 +0900

    Thymeleaf 인라인, 링크, 숫자 처리

--- src/main/java/org/zerock/ex2/controller/SampleController.java
+++ src/main/java/org/zerock/ex2/controller/SampleController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.ex2.dto.SampleDTO;
 
 import java.time.LocalDateTime;
@@ -22,7 +24,7 @@ public class SampleController {
         log.info("ex1...............");
     }
 
-    @GetMapping({"/ex2"})
+    @GetMapping({"/ex2", "/exLink"})
     public void exModel(Model model) {
 
         List<SampleDTO> list = IntStream.rangeClosed(1, 20)  // SampleDTO 타입 객체 20개 생성
@@ -40,5 +42,27 @@ public class SampleController {
                 }).collect(Collectors.toList());
 
         model.addAttribute("list", list);
+    }
+
+    @GetMapping({"/exInline"})
+    public String exInline(RedirectAttributes redirectAttributes) {
+        log.info("exInline...........");
+
+        SampleDTO dto = SampleDTO.builder()
+                .sno(400L)
+                .first("First...100")
+                .last("Last...100")
+                .regTime(LocalDateTime.now())
+                .build();
+
+        redirectAttributes.addFlashAttribute("result", "success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    public void ex3(){
+        log.info("ex3");
     }
 }
