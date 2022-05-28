cd070200e538a4ba061fab0d24dd07eb7f271b2c
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-05-28 22:43:06 +0900

    1. springboot version 2.7.0 --> 2.3.4 로 다운
    2. GuestBookController에서 list 추가
    
    3. list.html 추가

--- src/main/java/org/zerock/ex2/guestbook/controller/GuestBookController.java
+++ src/main/java/org/zerock/ex2/guestbook/controller/GuestBookController.java
@@ -1,18 +1,32 @@
 package org.zerock.ex2.guestbook.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.ex2.guestbook.dto.PageRequestDTO;
+import org.zerock.ex2.guestbook.service.GuestBookService;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestBookController {
 
-    @GetMapping({"/", "/list"})
-    public String list() {
-        log.info("list...........start");
-        return "/guestbook/list";
+    private final GuestBookService guestBookService;
+
+    @GetMapping("/")
+    public String index() {
+
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("list......" + pageRequestDTO);
+
+        model.addAttribute("result", guestBookService.getList(pageRequestDTO));
     }
 }
